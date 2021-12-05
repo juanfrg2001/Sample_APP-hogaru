a3497487c4980f6facb41c843b1e32918064c907
Development <you@example.com>
2021-12-05 18:28:59 -0500

    Sing up

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,6 @@ Rails.application.routes.draw do
   get 'home', to: 'static_pages#home'
 
   get 'signup', to: 'users#new'
+  resource :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
